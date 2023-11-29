846116b504ba8f9a79faaad03776ed96dfe68a86
pmk7 <72909830+pmk7@users.noreply.github.com>
2023-11-29 17:42:46 +0100

    add rate limiter to protect against malicious dos attacks

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import moviesRoutes from "./routes/moviesRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import listRoutes from "./routes/listRoutes.js";
+import { rateLimit } from "express-rate-limit";
 
 const port = process.env.PORT || 8000;
 
@@ -14,9 +15,20 @@ connectDB(); // Connect MongoDB
 
 const app = express();
 
+app.set("trust proxy", 1); // trust first proxy
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Use an external store for consistency across multiple server instances.
+});
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(limiter);
 
 // Cookie parser middleware
 app.use(cookieParser());
